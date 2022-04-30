990f864e11db754f2159033023de5c925d7412cd
Christopher Perez <chistoperez@gmail.com>
2022-04-30 16:42:45 -0500

    Adding feature Reader

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const { getStudents } = require("./lib/controller/StudentController");
+const Reader = require("./lib/utils/Reader");
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+app.get("/", (request, response) => {
+  response.json("Code Challenge LaunchX!");
+});
+
+app.get("/v1/students", (request, response) => {
+  const students = getStudents();
+  response.json(students);
+});
+
+app.get("/v1/students/email", (request, response) => {
+  const students = Reader.readJsonFile("db.json");
+
+  response.json(students);
+});
+
+app.get("/v1/students/credits", (request, response) => {
+  const students = Reader.readJsonFile("db.json");
+  response.json(students);
+});
+
+app.listen(port, () => {
+  console.log(`Student API in localhost:${port}`);
+});
